b26cc9b3d029868f0a81dfa3acb80592f7f01354
Jacob Boudreaux <166962113+Kernkob@users.noreply.github.com>
2026-03-31 16:30:10 +0000

    Created simple program which compares 2 lists of victors of a map and creates a new one

---
+++ Listcompare.py
@@ -0,0 +1,17 @@
+## Input ##
+initial_victors = input("List the initial victors (sep by \", \"): ")
+new_victors = input("List the GTR victors (sep by \", \"): ")
+
+## List edit ##
+
+initial_list = initial_victors.split(", ")
+new_list = new_victors.split(", ")
+
+for victor in new_list:
+    if victor not in initial_list:
+        initial_list.append(victor)
+
+new_str = ", ".join(initial_list)
+print()
+print("New list: ")
+print(new_str)
